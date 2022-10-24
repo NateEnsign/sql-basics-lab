bc8ae8b70e001de04d094004ec57c20cf5747768
Nate Ensign <dixienate@gmail.com>
2022-10-24 16:51:35 -0600

    artist complete

---
+++ sql-basics/artis.sql
@@ -0,0 +1,16 @@
+-- SELECT * FROM artist;
+
+-- INSERT INTO artist (name)
+-- VALUES ('Killers'),
+-- ('Modest Mouse'),
+-- ('Celine Deon');
+
+-- SELECT FROM artist WHERE artist_id <= 10
+-- ORDER BY name DESC;
+
+-- SELECT FROM artist WHERE artist_id <= 5
+-- ORDER BY name ASC;
+
+-- SELECT FROM artist WHERE name LIKE 'Black%';
+
+-- SELECT FROM artist WHERE name LIKE '%BLACK';
